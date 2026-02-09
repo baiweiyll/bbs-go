51f5b7a6b8e1d7bdbfcd900fed56333fd6058d3c
zhizhou.ren <zhizhou.ren@changhong.com>
2026-02-09 10:36:20 +0800

    302 redirect may be intercepted by nuxt

--- server/internal/server/router.go
+++ server/internal/server/router.go
@@ -79,6 +79,11 @@ func NewServer() {
 		m.Party("/install").Handle(new(api.InstallController))
 	})
 
+	mvc.Configure(app.Party("/oidc"), func(m *mvc.Application) {
+		m.Router.Use(middleware.Install)
+		m.Party("/login").Handle(new(api.OIDCController))
+	})
+
 	// admin
 	mvc.Configure(app.Party("/api/admin"), func(m *mvc.Application) {
 		m.Router.Use(middleware.Install)
